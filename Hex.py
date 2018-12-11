df116da33813a6fb1797944bc8cd49ca0e2b92bb
LeeCheer00 <645513992@qq.com>
2018-12-11 20:35:48 +0800

    function

---
+++ Hex.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=utf-8
+n1 = 255
+n2 = 1000
+a = print(hex(n1))
+
+b = print(hex(n2))
+print(a&b)
---
+++ my_abs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+def my_abs(x):
+    if x >= 0:
+        return 0
+    else:
+        return -x
+
+
+print(my_abs(-99))
